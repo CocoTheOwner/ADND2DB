5587c7b560d4504894820d3100978e72a3685c6c
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-11-23 19:39:00 +0100

    pog

--- src/main/java/nl/codevs/dndinventory/DnDInventory.java
+++ src/main/java/nl/codevs/dndinventory/DnDInventory.java
@@ -2,20 +2,30 @@ package nl.codevs.dndinventory;
 
 import nl.codevs.dndinventory.data.Item;
 import nl.codevs.dndinventory.data.Money;
+import nl.codevs.dndinventory.discord.DiscordIntegration;
 import nl.codevs.dndinventory.inventories.PlayerInventory;
 
+import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class DnDInventory {
-    private static final PlayerInventory inv =
+    public static final PlayerInventory inv =
             new PlayerInventory(
                     "Lazerus",
                     new ArrayList<>(),
                     new Money("69gp"),
                     75
             );
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException, LoginException, InterruptedException {
         inv.addItem(Item.Database.fromName("Sandals"), 1);
         System.out.println(inv.toJson());
+
+        String token = new BufferedReader(new FileReader("./token.txt")).readLine();
+        DiscordIntegration di = new DiscordIntegration(token);
     }
 }
--- src/main/java/nl/codevs/dndinventory/discord/DiscordIntegration.java
+++ src/main/java/nl/codevs/dndinventory/discord/DiscordIntegration.java
@@ -5,8 +5,11 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import nl.codevs.dndinventory.DnDInventory;
+import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
 import java.io.BufferedReader;
@@ -16,29 +19,31 @@ import java.util.Collections;
 
 public class DiscordIntegration extends ListenerAdapter
 {
-    public static void main(String[] args) throws LoginException, IOException, InterruptedException {
-        String token = new BufferedReader(new FileReader("./token.txt")).readLine();
 
-        JDA jda = JDABuilder.createLight(token, Collections.emptyList())
-                .addEventListeners(new DiscordIntegration())
+    private static final String PREFIX = "!";
+
+    private final Guild ourGuild;
+
+    private final JDA jda;
+
+    public DiscordIntegration(String token) throws LoginException, IOException, InterruptedException {
+
+        jda = JDABuilder.createLight(token, GatewayIntent.GUILD_MESSAGES)
+                .addEventListeners(this)
                 .setActivity(Activity.playing("Type /ping"))
                 .build();
 
         jda.upsertCommand("ping", "Calculate ping of the bot").queue(); // This can take up to 1 hour to show up in the client
         jda.awaitReady();
-        Guild ourGuild = jda.getGuildById(790135428228448286L);
-        assert ourGuild != null;
-        ourGuild.upsertCommand("ping", "Gop").queue();
+        ourGuild = jda.getGuildById(790135428228448286L);
     }
 
     @Override
-    public void onSlashCommand(SlashCommandEvent event)
-    {
-        if (!event.getName().equals("ping")) return; // make sure we handle the right command
-        long time = System.currentTimeMillis();
-        event.reply("Pong!").setEphemeral(true) // reply or acknowledge
-                .flatMap(v ->
-                        event.getHook().editOriginalFormat("Pong: %d ms", System.currentTimeMillis() - time) // then edit original
-                ).queue(); // Queue both reply and edit
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if (!event.getMessage().getContentStripped().startsWith(PREFIX)) return;
+
+        switch (event.getMessage().getContentStripped().substring(1)) {
+            case "inv" -> event.getChannel().sendMessage(DnDInventory.inv.getItems().get(0).getItem().toString()).queue();
+        }
     }
 }
